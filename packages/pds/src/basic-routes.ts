332ed52ab2f848392e49c68c59cc697882729a42
Denys Zarubin <denys.zarubin@vilmate.com>
2026-03-27 15:06:33 +0100

    Update basic-routes.ts

--- packages/pds/src/basic-routes.ts
+++ packages/pds/src/basic-routes.ts
@@ -7,27 +7,7 @@ export const createRouter = (ctx: AppContext): Router => {
 
   router.get('/', function (req, res) {
     res.type('text/plain')
-    res.send(`
-         __                         __
-        /\\ \\__                     /\\ \\__
-    __  \\ \\ ,_\\  _____   _ __   ___\\ \\ ,_\\   ___
-  /'__'\\ \\ \\ \\/ /\\ '__'\\/\\''__\\/ __'\\ \\ \\/  / __'\\
- /\\ \\L\\.\\_\\ \\ \\_\\ \\ \\L\\ \\ \\ \\//\\ \\L\\ \\ \\ \\_/\\ \\L\\ \\
- \\ \\__/.\\_\\\\ \\__\\\\ \\ ,__/\\ \\_\\\\ \\____/\\ \\__\\ \\____/
-  \\/__/\\/_/ \\/__/ \\ \\ \\/  \\/_/ \\/___/  \\/__/\\/___/
-                   \\ \\_\\
-                    \\/_/
-
-
-This is an AT Protocol Personal Data Server (aka, an atproto PDS)
-
-Most API routes are under /xrpc/
-
-      Code: https://github.com/bluesky-social/atproto
- Self-Host: https://github.com/bluesky-social/pds
-  Protocol: https://atproto.com
-`)
-  })
+    res.send(`This is an AT Protocol Personal Data Server hosted in Europe by WSocial.eu`)})
 
   router.get('/robots.txt', function (req, res) {
     res.type('text/plain')
